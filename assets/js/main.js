fea3edea2fd560b1e6886f48b60d7c7267180375
daoxuanphuong1208 <daoxuanphuong1208@gmail.com>
2024-11-27 17:21:42 +0700

    Orangy-Main: Refactor project to use sass

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function () {
+  new Splide("#header-slider", {
+    perPage: 2,
+    autoplay: false,
+    arrows: false,
+    pagination: false,
+    breakpoints: {
+      768: {
+        type: "loop",
+        perPage: 1,
+        arrows: true,
+        interval: 3000,
+        autoplay: true,
+      },
+    },
+  }).mount();
+
+  new Splide("#product-slider", {
+    perPage: 4,
+    autoplay: false,
+    arrows: false,
+    pagination: false,
+    breakpoints: {
+      1440: {
+        perPage: 3,
+      },
+
+      1024: {
+        perPage: 2.5,
+      },
+      768: {
+        type: "loop",
+        perPage: 1.5,
+      },
+    },
+  }).mount();
+});
